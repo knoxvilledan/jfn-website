25aa49a4b7fbff893ff04d73927d3e208e3d4ea7
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-22 10:43:18 -0400

    react change

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 
 
-function ComingSoonPage() {
-  return (
-    <div className="font-sans bg-gray-100 flex items-center justify-center min-h-screen text-black">
-      <Head>
-        <title>Coming Soon</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+import React from 'react';
 
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">My Website</h1>
-        <p className="text-xl mt-4">Coming soon...</p>
-      </div>
-    </div>
-  );
+const Home = () => {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <h1 className="text-4xl font-bold">Coming Soon!</h1>
+        </div>
+    );
 }
+
+export default Home;
