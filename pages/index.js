117db09a58f89a884bddf8ef5154836b2a5fd36d
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-22 00:50:41 -0400

    tailwind css test

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Head from 'next/head';
+// 2nd run at getting  a mvp up
 
 function ComingSoonPage() {
   return (
-    <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f9f9f9', textAlign: 'center', paddingTop: '20%' }}>
+    <div className="font-sans bg-gray-100 flex items-center justify-center min-h-screen text-black">
       <Head>
         <title>Coming Soon</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <h1>My Website</h1>
-      <p>Coming soon...</p>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">My Website</h1>
+        <p className="text-xl mt-4">Coming soon...</p>
+      </div>
     </div>
   );
 }
-
-export default ComingSoonPage;
