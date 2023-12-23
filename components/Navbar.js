3bdfe693052745b7ec9c0ed5d55d9b7e658c7409
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-12-23 17:44:50 -0500

    Refactor Navbar component and update README.md

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,24 @@
+const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/projects", label: "Projects" },
+  { href: "/blog", label: "Blog" },
+  { href: "/contact", label: "Contact" },
+];
+
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-gray-800 text-white p-4 shadow">
       <div className="container mx-auto flex justify-between">
         <div className="flex">
-          <a href="/" className="mr-4 hover:text-gray-300">
-            Home
-          </a>
-          <a href="/projects" className="mr-4 hover:text-gray-300">
-            Projects
-          </a>
-          <a href="/blog" className="hover:text-gray-300">
-            Blog
-          </a>
-        </div>
-        <div>
-          <a href="/contact" className="hover:text-gray-300">
-            Contact
-          </a>
+          {navLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              className="mr-4 hover:text-gray-300"
+            >
+              {link.label}
+            </a>
+          ))}
         </div>
       </div>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import Navbar from "../components/Navbar";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
