0ba773b11616198ad6be6d95545a9da934c4bb5b
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-12-24 16:31:29 -0500

    Add Navbar styling and About page component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,23 +1,27 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
 const navLinks = [
   { href: "/", label: "Home" },
   { href: "/projects", label: "Projects" },
   { href: "/blog", label: "Blog" },
   { href: "/contact", label: "Contact" },
+  { href: "/about", label: "About" },
 ];
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 shadow">
+    <nav className={`bg-gray-800 text-white p-4 shadow ${styles.lightning}`}>
       <div className="container mx-auto flex justify-between">
         <div className="flex">
           {navLinks.map((link, index) => (
-            <a
-              key={index}
+            <Link
               href={link.href}
+              key={index}
               className="mr-4 hover:text-gray-300"
             >
               {link.label}
-            </a>
+            </Link>
           ))}
         </div>
       </div>
--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const aboutMe = {
   name: "Frantasia Hogan",
   profession: "Masseuse",
@@ -10,12 +12,30 @@ const aboutMe = {
 };
 
 // Add your own details
-aboutMe.name = "Your Name";
-aboutMe.origin = "Chicago";
+// aboutMe.name = "Your Name";
+// aboutMe.origin = "Chicago";
 
 // Add people to your network
-aboutMe.addToNetwork("Person 1");
-aboutMe.addToNetwork("Person 2");
-aboutMe.addToNetwork("Person 3");
+// aboutMe.addToNetwork("Person 1");
+// aboutMe.addToNetwork("Person 2");
+// aboutMe.addToNetwork("Person 3");
 
-console.log(aboutMe);
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Name: {aboutMe.name}</p>
+      <p>Profession: {aboutMe.profession}</p>
+      <p>Location: {aboutMe.location}</p>
+      <p>Origin: {aboutMe.origin}</p>
+      <h2>Network</h2>
+      <ul>
+        {aboutMe.network.map((person, index) => (
+          <li key={index}>{person}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default About;
