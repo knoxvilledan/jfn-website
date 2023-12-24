a69d2f7f85b3df60a1280b6ef9a978baaf7ad508
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-12-23 19:02:36 -0500

    Add HeroSection component and Tailwind CSS plugins

---
+++ components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <h1>Welcome to the Hero Section!</h1>
+      <p>
+        This is where you can showcase important information or call-to-action.
+      </p>
+    </div>
+  );
+};
+
+export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./home.module.css";
+import HeroSection from "../components/HeroSection"; // Add this line
 
 const Home = () => {
   return (
     <div className={styles.background}>
+      <HeroSection /> {/* Add this line */}
       <div className={styles.container}>
         <h1 className="text-4xl font-bold animate-pulse">Under Construction</h1>
         <h2 className="text-2xl mt-4 animate-bounce">Coming Soon!</h2>
