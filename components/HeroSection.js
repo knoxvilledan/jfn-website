bbfc018a906d2a0a4a8b48f3fa3fdb10a8c78b4f
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-12-24 14:55:47 -0500

    Add ESLint configuration and update package dependencies

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,7 +4,9 @@ const HeroSection = () => {
   return (
     <div className="hero-section">
       <h1>Frantasia Hogan</h1>
-      <p>Brand New Massage Therapist. Means I'm still learning and growing.</p>
+      <p>
+        Brand New Massage Therapist. Means I&apos;m still learning and growing.
+      </p>
     </div>
   );
 };
