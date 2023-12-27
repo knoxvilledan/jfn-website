0efc77ddfe483b6e93704754536b97484b72bd6c
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-12-26 20:09:19 -0500

    Update HeroSection and Navbar styles

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="hero-section">
+    <div className="hero-section mb-6">
       <h1>Frantasia Hogan</h1>
       <p>
         Brand New Massage Therapist. Means I&apos;m still learning and growing.
